2140f49c94f1c84f5698a4ba9ca1ea0105a76e2b
Mark Arkhipychev <hackyourrself@gmail.com>
2023-07-12 21:19:58 +0300

    Saves

--- app/tasks/celery_app.py
+++ app/tasks/celery_app.py
@@ -6,7 +6,6 @@ celery = Celery(
     broker=f"redis://{settings.REDIS_HOST}:{settings.REDIS_PORT}",
     backend=f"redis://{settings.REDIS_HOST}:{settings.REDIS_PORT}",
     include=[
-        "app.tasks.tasks",
         "app.MoySklad.tasks",
         "app.MoySklad.items.tasks",
         "app.MoySklad.sales.tasks",
