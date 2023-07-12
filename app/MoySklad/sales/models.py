c385786b857d0d1e7a86a14b8f355774df95da11
Mark Arkhipychev <hackyourrself@gmail.com>
2023-07-12 21:01:11 +0300

    Saves

--- app/MoySklad/sales/models.py
+++ app/MoySklad/sales/models.py
@@ -1,19 +1,25 @@
-from sqlalchemy import Column, Integer, Date, ForeignKey, Computed
+from sqlalchemy import Column, Integer, Date, String, ForeignKey
 
 from app.database import Base
+from app.users.models import Users
 
 
-class Sales(Base):
-    __tablename__ = 'sales'
+class Orders(Base):
+    __tablename__ = 'orders'
 
     id = Column(Integer, primary_key=True)
-    item_id = Column(ForeignKey('items.id'), nullable=False)
-    sale_date = Column(Date, nullable=False)
-    quantity = Column(Integer, nullable=False)
-    price = Column(Integer, nullable=False)
-    total_cost = Column(Integer, Computed("quantity * price"))
+    ms_id = Column(String, unique=True, nullable=False)
+    user_id = Column(ForeignKey('users.id'), nullable=False)
+    order_date = Column(Date, nullable=False)
 
-    def __str__(self):
-        return f"Sale id: {self.id}"
+
+class OrderDetails(Base):
+    __tablename__ = 'order_details'
+
+    id = Column(Integer, primary_key=True)
+    order_ms_id = Column(ForeignKey('orders.ms_id'), nullable=False)
+    product_ms_id = Column(ForeignKey('items.ms_id'), nullable=False),
+    quantity = Column(Integer, nullable=False)
+    sum = Column(Integer, nullable=False)
 
 
