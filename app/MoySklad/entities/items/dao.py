090fdda6410c30eaf941a82ad2ad3194a0ef247b
Mark Arkhipychev <hackyourrself@gmail.com>
2023-07-25 17:34:42 +0300

    Replaced DAO methods to parental

--- app/MoySklad/entities/items/dao.py
+++ app/MoySklad/entities/items/dao.py
@@ -1,16 +1,7 @@
 from app.dao.base import BaseDAO
 from app.MoySklad.entities.items.models import Items
-from sqlalchemy import insert
-
-from app.database import async_session_maker
 
 
 class ItemsDAO(BaseDAO):
     model = Items
 
-    @classmethod
-    async def add_items(cls, items: list):
-        async with async_session_maker() as session:
-            query = insert(cls.model).values(items)
-            await session.execute(query)
-            await session.commit()
--- app/MoySklad/orders/dao.py
+++ app/MoySklad/orders/dao.py
@@ -1,26 +1,10 @@
 from app.dao.base import BaseDAO
 from app.MoySklad.orders.models import Orders, OrderDetails
-from app.database import async_session_maker
-from sqlalchemy import insert
 
 
 class OrdersDAO(BaseDAO):
     model = Orders
 
-    @classmethod
-    async def add_orders(cls, items: list):
-        async with async_session_maker() as session:
-            query = insert(cls.model).values(items)
-            await session.execute(query)
-            await session.commit()
-
 
 class OrderDetailsDAO(BaseDAO):
     model = OrderDetails
-
-    @classmethod
-    async def add_order_details(cls, items: list):
-        async with async_session_maker() as session:
-            query = insert(cls.model).values(items)
-            await session.execute(query)
-            await session.commit()
