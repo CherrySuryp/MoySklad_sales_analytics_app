5559ffb8e519816b40cfc165b2d3ba8a5d5c53cb
Mark Arkhipychev <hackyourrself@gmail.com>
2023-07-26 15:44:16 +0300

    Trying to fix "Sever Disconnected Error" by limiting RPS

--- app/MoySklad/orders/test_async_req.py
+++ app/MoySklad/orders/test_async_req.py
@@ -22,50 +22,50 @@ async def make_request(session, ms_id):
             return result
 
 
+def slice_list_to_chunks(to_slice: list, chunk_size: int):
+    for piece in range(0, len(to_slice), chunk_size):
+        yield to_slice[piece:piece + chunk_size]
+
+
 async def send_requests():
+    start_time = time.time()
+
+    result = []
     with open('dataset.json', 'r', encoding='utf8') as f:
         ids = json.load(f)
-        print(f'{len(ids)} requests per chunk')
-    async with aiohttp.ClientSession() as session:
-        tasks = []
-        request_interval = 3 / 45  # Интервал между запросами в секундах
 
-        for ms_id in tqdm(ids, desc='Async gather fetching data...', colour='GREEN'):
-            task = asyncio.ensure_future(make_request(session, ms_id))
-            tasks.append(task)
-            await asyncio.sleep(request_interval)  # Ожидание перед следующим запросом
+    chunk_size = 200
+    chunks = list(slice_list_to_chunks(ids, chunk_size))
 
-        responses = await asyncio.gather(*tasks)
-        responses = [i for i in responses]
-        print('Finished making requests')
-        res = []
-        for i in responses:
-            res.extend(i)
+    print(f'{len(ids)} requests to make')
+    print(f"{chunk_size} requests per chunk")
 
-        with open('res.json', 'w', encoding='utf8') as f:
-            json.dump(res, f, ensure_ascii=False, indent=2)
-        return len(res)
+    for chunk in tqdm(chunks, desc='Proceeding chunks...', colour='GREEN'):
+
+        async with aiohttp.ClientSession() as session:
+
+            tasks = []
+            request_interval = 3 / 45  # Интервал между запросами в секундах
+
+            for ms_id in chunk:
+                task = asyncio.ensure_future(make_request(session, ms_id))
+                tasks.append(task)
+                await asyncio.sleep(request_interval)  # Ожидание перед следующим запросом
+
+            result.extend(await asyncio.gather(*tasks))
+
+            await session.close()
+
+    with open('res.json', 'w', encoding='utf8') as f:
+        json.dump(result, f, ensure_ascii=False, indent=2)
+
+    elapsed_time = time.time() - start_time
+
+    print(f"Execution time: {elapsed_time} secs.")
+    print(f"Requests made: {len(ids)}")
+    print(f"Requests per async task: {chunk_size}")
+    print(f'Actual RPS: {len(ids) / elapsed_time}')
 
 
-def partition(n):
-    with open('dataset.json', 'r', encoding='utf8') as file:
-        ids = json.load(file)
-
-    for piece in range(0, len(ids), n):
-        yield ids[piece:piece + n]
-
-
-# slices = list(partition(200))
-# for i in slices:
-#     print(len(i))
-#
-# with open('res.json', 'w', encoding='utf8') as f:
-#     json.dump(slices, f, ensure_ascii=False, indent=2)
-
-start_time = time.time()
 loop = asyncio.get_event_loop()
-results = loop.run_until_complete(send_requests())
-elapsed_time = time.time() - start_time
-
-print(f"Время выполнения: {elapsed_time} сек.")
-print(results)
+loop.run_until_complete(send_requests())
